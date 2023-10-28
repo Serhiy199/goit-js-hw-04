e48c1f9c5ef7a0a08845c995270b0fc4af72d676
Serhiy199 <bryklin.best.1994@gmail.com>
2023-10-28 19:14:11 +0300

    finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,19 @@
 'use strict';
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customerCredits) {
-    return 'Insufficient funds!';
-  } else {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
+function isEnoughCapacity(products, containerSize) {
+    let total = 0;
+
+    for (const product in products) {
+        total += products[product];
+    }
+
+    return Boolean(total < containerSize);
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,30 @@
 'use strict';
 
-function checkForSpam(message) {
-  let sale = 'SalE';
-  let spam = 'spam';
-  const lowercaseInputMessage = message.toLowerCase();
-  const lowercaseInputSale = sale.toLowerCase();
-  const lowercaseInputSpam = spam.toLowerCase();
-  return (
-    lowercaseInputMessage.includes(lowercaseInputSale) ||
-    lowercaseInputMessage.includes(lowercaseInputSpam)
-  );
-}
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+const profile = {
+    username: 'Jacob',
+    playTime: 300,
+
+    getPotions() {
+        return this.potions;
+    },
+
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours) {
+        this.playTime = this.playTime + hours;
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
